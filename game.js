dbf72e0250802d758d86ab77a18b5bdfa024e2ca
Marcos Salazar <Marcos1010521@gmail.com>
2020-11-14 20:41:22 -0400

    now mosquitos have diferent sizes

--- game.js
+++ game.js
@@ -26,11 +26,24 @@ function randomPosition(){
     //Criando elemento img e posicionando no body
     var mosquito = document.createElement('img')
     mosquito.src = 'image/mosca.png'
-    mosquito.className = 'mosquito1'
+    mosquito.className = randomSize()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
 
     document.body.appendChild(mosquito)
 
+}
+
+function randomSize(){
+    var classe = Math.floor(Math.random()*3);
+    
+    switch(classe){
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }   
 }

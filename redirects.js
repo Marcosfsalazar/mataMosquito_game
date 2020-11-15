3d38608c329864476fef9b72c668935cf4cf88d6
Marcos Salazar <Marcos1010521@gmail.com>
2020-11-14 22:32:48 -0400

    implement level selector

--- redirects.js
+++ redirects.js
@@ -1,3 +1,16 @@
+
+var gamePage = 'app.html'
+var mainPage = 'index.html'
+
+function startGame(){
+    var level = document.getElementById('level').value
+    if(level == ''){
+        alert('Selecione um nível!')
+        return false;
+    }
+    alert(level)
+}
+
 function playAgain(){
-    window.location.href = 'index.html'
+    window.location.href = 'app.html'
 }
